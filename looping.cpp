612dd696dc16fe9002e2c399b4c6e66f1e88b604
Thaddea4 <adeyasyie@gmail.com>
2026-03-30 19:16:53 +0700

    Menambahkan perulangan FOR

--- looping.cpp
+++ looping.cpp
@@ -8,5 +8,6 @@ int main (){
     //Perulangan dengan FOR
     //mencetak tulisan sebanyak 5 kali
     cout<<"PERULANGAN FOR"<<endl;
-
-}
+ for(int i=0;i<=4;i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
