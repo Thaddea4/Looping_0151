1b352721d6e243e563cae6506cb69000f059ee70
Thaddea4 <adeyasyie@gmail.com>
2026-03-30 20:04:46 +0700

    Menambahkan logika dengan AND untuk cek kelulusan

--- looping.cpp
+++ looping.cpp
@@ -61,6 +61,7 @@ float nilB,nilM,rerata;
 
     cout<<"Status kelulusan = "<<status<<", dengan nilai rata-rata = "<<rerata<<endl;
 }
+
 Logika OR
 #include <iostream>
 using namespace std;
@@ -77,6 +78,29 @@ int main(){
     rerata = (nilB+nilM)/2;
 
     if ((rerata >= 60 ) || (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<", dengan nilai rata-rata = "<<rerata<<endl;
+}
+
+Logika AND
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60 ) && (nilM >= 70))
         status = "Lulus";
     else
         status = "Tidak lulus";
