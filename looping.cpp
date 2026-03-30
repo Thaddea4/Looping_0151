5b8751378f874806083b9ef8bb5a44cdb45bfc1a
Thaddea4 <adeyasyie@gmail.com>
2026-03-30 19:58:50 +0700

    Menambahkan logika dengan NOT untuk cek kelulusan

--- looping.cpp
+++ looping.cpp
@@ -27,7 +27,6 @@ int main (){
     cout<<"Bilangan acak while yang terakhir ="<<x<<endl;
     cout<<endl;
 
-    
     //Perulangan dengan DO...WHILE
     cout<<"PERULANGAN DO...WHILE"<<endl;
     x = 1 + rand()%10;
@@ -38,7 +37,6 @@ int main (){
 
     cout<<"Bilangan acak do-while yang terakhir ="<<x<<endl;
     cout<<endl;
-
 }
 
 Logika NOT 
@@ -46,5 +44,20 @@ Logika NOT
 using namespace std;
 
 int main(){
+float nilB,nilM,rerata;
+    string status;
 
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if (!(rerata < 60 ))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<", dengan nilai rata-rata = "<<rerata<<endl;
 }
